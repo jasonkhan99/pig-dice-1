aa79e9171ccd5e69ea944c9bdae1576388166862
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-06-17 14:02:48 -0700

    Add player1 and player2 buttons to index.html, add initial styling to buttons, add user interface logic to scripts.js.
    co-authored-by: Jason Khan <jasonderekkhan@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,9 @@ AddUser.prototype.diceRoll = function() {
 
 AddUser.prototype.calculateTotalScore = function() {
   this.totalScore += this.turnScore;
+  this.turnScore = 0;
   if (this.totalScore >= 100) {
+    this.totalScore = 0;
     alert("Game Over, Someone Wins!");
   }
 }
@@ -26,3 +28,22 @@ let player1 = new AddUser(0, 0, 0);
 let player2 = new AddUser(0, 0, 0);
 
 //user interface logic
+
+$(document).ready(function() {
+  $("#player1roll").click(function() {
+
+  });
+
+  $("#player1hold").click(function() {
+
+  });
+
+
+  $("#player2roll").click(function() {
+
+  });
+
+  $("#player2hold").click(function() {
+
+  });
+});
