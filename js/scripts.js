3f18eebf9fdb71b19a726c8e1063227a4068875d
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-06-17 11:31:37 -0700

    Add to scripts.js business logic, diceRoll custom method.
    co-authored-by: Jason Khan <jasonderekkhan@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,12 @@ function AddUser(totalScore, turnScore, rollValue) {
   this.rollValue = rollValue;
 }
 
+AddUser.prototype.diceRoll = function() {
+  let rollValue = Math.floor(Math.random() * 6) + 1;
+  console.log(rollValue);
+}
 
-
-
-
-
+let player1 = new AddUser();
+let player2 = new AddUser();
 
 //user interface logic
