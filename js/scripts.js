1112e5aaa11befeff9763e007d63782e1f21b58a
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-06-17 12:05:18 -0700

    Attempting to resolve exception in scrips.js, error states Uncaught ReferenceError: rollValue is not defined.
    co-authored-by: Jason Khan <jasonderekkhan@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,28 @@
 //business logic to create user object
-function AddUser(totalScore, turnScore, rollValue) {
+function AddUser(totalScore, turnScore) {
   this.totalScore = totalScore;
   this.turnScore = turnScore;
-  this.rollValue = rollValue;
+  // this.rollValue = rollValue;
 }
 
 AddUser.prototype.diceRoll = function() {
   let rollValue = Math.floor(Math.random() * 6) + 1;
+  return rollValue;
+  if (rollValue === 1) {
+    player1.turnScore = 0;
+  } else {
+    player1.turnScore = turnScore + rollValue;
+  }
   console.log(rollValue);
 }
 
+//prototype.decideAction(currentDiceValue) {
+
+//}
+
 let player1 = new AddUser();
 let player2 = new AddUser();
+// let currentDiceValue = player1.diceRoll();
+// console.log(player1.diceRoll());
 
 //user interface logic
