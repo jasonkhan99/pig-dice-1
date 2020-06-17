9ff63db0a91faa826a38f816260f43cbcc2bbc65
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-06-17 10:40:45 -0700

    add a user object constructor
    Co-authored-by: Brittany Lindgren <brittdevelopsit@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+//business logic to create user object
+function AddUser(totalScore, turnScore, rollValue) {
+  this.totalScore = totalScore;
+  this.turnScore = turnScore;
+  this.rollValue = rollValue;
+}
+
+
+
+
+
+
+
+//user interface logic
