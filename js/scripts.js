d44ee9c7c764659c627a519176d5fadc3b9a8f70
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-06-18 13:16:17 -0700

    Add custom method to check if there is a winner after each player roll.
    co-authored-by: Jason Khan <jasonderekkhan@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -20,11 +20,18 @@ AddUser.prototype.diceRoll = function() {
 AddUser.prototype.calculateTotalScore = function() {
   this.totalScore += this.turnScore;
   this.turnScore = 0;
-  if (this.totalScore >= 100) {
-    this.totalScore = 0;
-    alert("Game Over, Someone Wins!");
-  }
+  this.rollValueArray = [];
+  // if (this.totalScore >= 100) {
+  //   this.totalScore = 0;
+  //   alert("Game Over, Someone Wins!");
+  // }
   return this.totalScore;
+}
+
+AddUser.prototype.checkForWinner = function() {
+  if (this.turnScore + this.totalScore >= 100) {
+    alert("Game Over");
+  }
 }
 
 let player1 = new AddUser(0, 0, 0);
@@ -34,10 +41,9 @@ let player2 = new AddUser(0, 0, 0);
 //user interface logic
 
 $(document).ready(function() {
-
   $("#player1roll").click(function() {
     let rollResult1 = player1.diceRoll();
-    $("#player1-roll-message").show();
+    $("#player1InitiallyHidden").show();
     $("#player1dice").text(rollResult1);
     $("#player1TotalDice").text(player1.turnScore);
     if(rollResult1 === 1){
@@ -45,7 +51,8 @@ $(document).ready(function() {
       $(".not-player1s-turn").show();
       $(".not-player2s-turn").hide();
       $(".player2buttons").show();
-    } 
+    }
+    player1.checkForWinner();
   });
 
   $("#player1hold").click(function() {
@@ -60,13 +67,16 @@ $(document).ready(function() {
 
   $("#player2roll").click(function() {
     let rollResult2 = player2.diceRoll();
+    $("#player2InitiallyHidden").show();
     $("#player2dice").text(rollResult2);
+    $("#player2TotalDice").text(player2.turnScore);
     if(rollResult2 === 1){
       $(".player2buttons").hide();
       $(".not-player2s-turn").show()
       $(".not-player1s-turn").hide();
       $(".player1buttons").show();
     }
+    player2.checkForWinner();
   });
 
   $("#player2hold").click(function() {
@@ -76,5 +86,5 @@ $(document).ready(function() {
     $(".not-player2s-turn").show()
     $(".not-player1s-turn").hide();
     $(".player1buttons").show();
-  });
+  });  
 });
